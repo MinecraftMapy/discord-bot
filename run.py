585ed0f60e958051596a5a53e5d2d8bd123aabf7
PanKlipcio <21959209+PanKlipcio@users.noreply.github.com>
2020-04-27 11:31:52 +0200

    Using f-strings instead of .format() [Python 3.6+] (#1)

--- run.py
+++ run.py
@@ -21,37 +21,35 @@ async def fetch(session, endpoint):
 @bot.command()
 async def profil(ctx, username):
     async with aiohttp.ClientSession(headers=headers) as session:
-        response = await fetch(session, '/user/{}'.format(username))
+        response = await fetch(session, f'/user/{username}')
     data = response['data']
 
     embed = Embed(
-        title='[Profil] {}'.format(data['info']['username']),
+        title=f'[Profil] {data["info"]["username"]}',
         description=data['info']['description'],
         colour=Colour.from_rgb(0, 144, 255),
         url=data['url'],
     )
 
-    info = """
-    **Rola:** {}
-    **Aktywny:** {}
-    **Napisanych komentarzy:** {}
-    **Oddanych diamentów:** {}
-    """.format(data['info']['role'], data['info']['last_logged_relative'], data['stats']['written_comments'],
-               data['stats']['given_diamonds'])
+    info = f"""
+    **Rola:** {data['info']['role']}
+    **Aktywny:** {data['info']['last_logged_relative']}
+    **Napisanych komentarzy:** {data['stats']['written_comments']}
+    **Oddanych diamentów:** {data['stats']['given_diamonds']}
+    """
 
     embed.add_field(name='Informacje', value=info, inline=True)
 
-    stats = """
-    <:diax:589865302100017172> Diamenty: {}
-    <:gwiazdka:589865302230040576> Obserwujących: {}
-    <:mapa:589865302020456456> Wrzuconych map: {}
-    """.format(data['stats']['diamond_sum'], data['stats']['follower_count'], data['stats']['map_count'])
+    stats = f"""
+    <:diax:589865302100017172> Diamenty: {data['stats']['diamond_sum']}
+    <:gwiazdka:589865302230040576> Obserwujących: {data['stats']['follower_count']}
+    <:mapa:589865302020456456> Wrzuconych map: {data['stats']['map_count']}
+    """
 
     embed.add_field(name='Statystyki', value=stats, inline=True)
 
     embed.set_thumbnail(url=data['info']['avatar_url'])
-    embed.set_footer(text='Invoked by {}#{} • {}'.format(ctx.author.name, ctx.author.discriminator, ctx.author.id),
-                     icon_url=ctx.author.avatar_url)
+    embed.set_footer(text=f'Invoked by {ctx.author} • {ctx.author.id}', icon_url=ctx.author.avatar_url)
 
     await ctx.send(embed=embed)
 
